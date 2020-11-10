9a714bf82bc01b76205f559d0e608d21dc33882b
Olivia Leggio <o.r.legg12@gmail.com>
2020-11-10 13:24:56 -0500

    tested command parameters, gets player display name

--- src/com/worminthedirt/farmhelper/Hai.java
+++ src/com/worminthedirt/farmhelper/Hai.java
@@ -9,9 +9,38 @@ import org.bukkit.entity.Player;
 
 public class Hai implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            player.sendMessage(ChatColor.YELLOW + "haiii :3c");
+        if (args.length == 0) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                player.sendMessage("haiii " + player.getDisplayName() + " :3c");
+            }
         }
+        else if (args[0].equals("yellow")) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                player.sendMessage(ChatColor.YELLOW + "haiii " + player.getDisplayName() + " :3c");
+            }
+        }
+        else if (args[0].equals("green")) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                player.sendMessage(ChatColor.GREEN + "haiii " + player.getDisplayName() + " :3c");
+            }
+        }
+        else if (args[0].equals("red")) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                player.sendMessage(ChatColor.RED + "haiii " + player.getDisplayName() + " :3c");
+            }
+        }
+        else if (args[0].equals("blue")) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                player.sendMessage(ChatColor.BLUE + "haiii " + player.getDisplayName() + " :3c");
+            }
+        }
+        else {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                player.sendMessage("haiii " + player.getDisplayName() + " :3c");
+            }
+        }
+
+
         return true;
     }
 }
